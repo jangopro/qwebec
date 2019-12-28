20fff0724dc82c612b2bf2e56426a88a165aaf24
Michael Provencher <michaelprovencher15@gmail.com>
2019-12-28 00:56:23 -0500

    communication between frontend and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,31 @@
-import React from "react";
-import "./App.scss";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import EventDetails from "./components/EventDetails";
+import React from 'react';
+import './App.scss';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import EventDetails from './components/EventDetails';
+
+import axios from 'axios';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header></Header>
-        <main>
-          <div className="container">
-            <Switch>
-              <Route path="/event/:id" component={EventDetails} />
-              <Route exact path="/" component={Home} />
-            </Switch>
-          </div>
-        </main>
-        <Footer></Footer>
-      </BrowserRouter>
-    </div>
-  );
+    axios.get('/api/hello').then(data => console.log(data));
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header></Header>
+                <main>
+                    <div className="container">
+                        <Switch>
+                            <Route path="/event/:id" component={EventDetails} />
+                            <Route exact path="/" component={Home} />
+                        </Switch>
+                    </div>
+                </main>
+                <Footer></Footer>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
