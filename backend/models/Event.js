db10fd9973e017af6b8de0bc73868cc606736c6e
Michael Provencher <michaelprovencher15@gmail.com>
2020-02-09 11:14:40 -0500

    added todo

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+// TODO: Code postal
+// TODO: author pour organizers?
+// TODO Le nom de l'endroit? ex: Centre de foires
 
 const eventSchema = new mongoose.Schema({
     name: String,
